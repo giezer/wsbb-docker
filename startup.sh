052a51b6cea0a79cf93ebcedb81fb71db448eff9
Fábio Rodrigues Ribeiro <farribeiro@gmail.com>
2018-01-27 00:28:50 -0200

    Use sudo instead su

--- startup.sh
+++ startup.sh
@@ -17,9 +17,9 @@ fi
 if [ ! -d ~/.mozilla ]
 then
   firefox -CreateProfile default \
-  && su -c "apt update && apt -y upgrade && apt -y install /src/GBPCEFwr64.deb"
+  && sudo "apt update && apt -y upgrade && apt -y install /src/GBPCEFwr64.deb"
 else
-  su -c "/etc/init.d/warsaw start"
+  sudo "/etc/init.d/warsaw start"
 fi
 
 /usr/local/bin/warsaw/core \
