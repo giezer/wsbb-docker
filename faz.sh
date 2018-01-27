31904e54d9e62231b781abd300ee6f0f2322c727
jsalatiel <jsalatiel@users.noreply.github.com>
2018-01-27 09:32:53 -0300

    Update faz.sh

--- faz.sh
+++ faz.sh
@@ -1,10 +1,13 @@
+echo '**********************************'
 echo Vai apagar TODOS os containers ...
+echo '**********************************'
 echo '^C para cancelar'
+
 read
 
-docker ps -a | grep -v IMAGE | awk '{print $1}'|  xargs  -r -n1 docker stop
-docker ps -a | grep -v IMAGE | awk '{print $1}'|  xargs  -r -n1 docker rm
-docker images | grep -i 'ws\|none' | awk '{print $3}'|  xargs  -r -n1 docker rmi
+docker ps -a  | grep -i 'wsbb\|none' | awk '{print $1}'|  xargs  -r -n1 docker stop
+docker ps -a  | grep -i 'wsbb\|none' | awk '{print $1}'|  xargs  -r -n1 docker rm
+docker images | grep -i 'wsbb\|none' | awk '{print $3}'|  xargs  -r -n1 docker rmi
 
 docker-compose build wsbb
 docker-compose run --name wsbb wsbb 
