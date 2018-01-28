f6dce2d9073945a945ec46d6ea6838c082e4944c
jsalatiel <jsalatiel@users.noreply.github.com>
2018-01-28 13:47:05 -0300

    Update build_and_run.sh

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
+
 if [ $(id -u) == 0 ] ; then
-  echo Nao vai rodar como root
-  echo Adicione seu usuario no grupo docker
+  echo Esse programa nao deve ser executado como root
   exit 1
 fi
 
-if ! ( which docker &>/dev/null && which docker-compose &>/dev/null ) ; then
-	echo Faltando dependencias...
-	echo
-	echo Para instalar o docker use:
-	echo \'sudo apt-get install docker.io -y\' em variantes do debian  ou \'sudo yum install -y docker\' em variantes do redhat
-	echo Para instalar o docker-compose use:
+if ! ( groups $1 | grep docker &>/dev/null) ; then
+	echo Voce nao tem permissao para rodar comandos no docker. Se adicione no grupo docker usando:
+	echo \'"sudo usermod -aG docker $USER"\' depois deslogue e logue novamente.
+	exit 2
+fi
+
+if ! which docker &>/dev/null ; then
+	echo docker nao esta instalado. Instale usando:
+	echo \'sudo apt-get install docker.io -y\' em variantes do ubuntu  ou \'sudo yum install -y docker.ce\' em variantes do CentOS
+	exit 3
+fi
+
+if ! which docker-compose &>/dev/null  ; then
+	echo docker-compose nao instalado. Instale usando:
 	echo \'"sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose && sudo chmod +x /usr/local/bin/docker-compose"\'
-	exit 1
+	exit 4
 fi
 
 docker start wsbb  &>/dev/null || (
