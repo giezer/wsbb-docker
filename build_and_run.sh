458ea93b04f5610209a310b2ddc027f3d68e3433
jsalatiel <jsalatiel@users.noreply.github.com>
2018-01-28 13:21:26 -0300

    Update build_and_run.sh

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
-
 if [ $(id -u) == 0 ] ; then
   echo Nao vai rodar como root
   echo Adicione seu usuario no grupo docker
   exit 1
+fi
+
+if ! ( which docker &>/dev/null && which docker-compose &>/dev/null ) ; then
+	echo Faltando dependencias...
+	echo
+	echo Para instalar o docker use:
+	echo \'sudo apt-get install docker.io -y\' em variantes do debian  ou \'sudo yum install -y docker\' em variantes do redhat
+	echo Para instalar o docker-compose use:
+	echo \'"sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose && sudo chmod +x /usr/local/bin/docker-compose"\'
+	exit 1
 fi
 
 docker start wsbb  &>/dev/null || (
