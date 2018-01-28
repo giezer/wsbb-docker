efda1d95065ae43e0b561aa27c7543c91219f078
jsalatiel <jsalatiel@users.noreply.github.com>
2018-01-28 13:05:16 -0300

    Update build_and_run.sh

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+if [ $(id -u) == 0 ] ; then
+  echo Nao vai rodar como root
+  echo Adicione seu usuario no grupo docker
+  exit 1
+fi
+
 docker start wsbb  &>/dev/null || (
 docker ps -a  | grep -i 'wsbb' | awk '{print $1}'|  xargs  -r -n1 docker stop
 docker ps -a  | grep -i 'wsbb' | awk '{print $1}'|  xargs  -r -n1 docker rm
